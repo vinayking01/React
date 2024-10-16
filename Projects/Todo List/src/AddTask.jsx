661231c5f6d525b347e64c2f59e6d4d0857e4c5c
Vinay Singh <vinaysinghcktd01@gmail.com>
2024-10-16 18:49:38 +0530

    " local Storage added"

--- Projects/Todo List/src/AddTask.jsx
+++ Projects/Todo List/src/AddTask.jsx
@@ -4,21 +4,36 @@ import { ClearAll } from "./ClearAllList";
 
 export function AddTask() {
     var [title, SetTask] = useState('');
-    var [TaskList, SetList] = useState([]);
-    var [TaskLength, SetLength] = useState(0);
+
+    // Now we are taking list data from the local Storage.
+    var [TaskList, SetList] = useState(()=>{
+       var rawList = localStorage.getItem('Data_OnLocalStorage');
+       if(!rawList) // because initially the local storage area is blank so it should return blank array
+        {
+            return [];
+        }
+        return JSON.parse(rawList); 
+    }); 
+
+    var [TaskLength, SetLength] = useState((JSON.parse(localStorage.getItem('Data_OnLocalStorage')))? JSON.parse(localStorage.getItem('Data_OnLocalStorage')).length:0);  // taking the latest length of the tsk list using local storage
+    
 
     function updateTask(event) {
         // console.log(event.target.value);
         SetTask(event.target.value);
+        
     }
     function DisplayTask(event) {
         event.preventDefault(); // Prevent the form from submitting and reloading the page
 
         if (!TaskList.includes(title) && title !== '') {
             SetList([...TaskList, title])
+             // Adding list to local Storage
+            localStorage.setItem('Data_OnLocalStorage', JSON.stringify([...TaskList, title]));    // adding the every new task in our local storage.
             SetLength(TaskLength + 1);
             SetTask('')
-            // console.log(TaskList)
+           
+            
         }
         
     }
@@ -26,12 +41,15 @@ export function AddTask() {
     const deleteTask = (curElem) => {
         var NewList = TaskList.filter(Element => ((curElem) != Element));
         SetList(NewList);
+        localStorage.setItem('Data_OnLocalStorage', JSON.stringify(NewList));       // After deleting the selective task updating the local storage.
     };
      function ClearALList()
     {
         SetList([]);
+        localStorage.setItem('Data_OnLocalStorage', '[]');             // After deleting all the task updating the local storage.
         SetLength(0);
     }
+
     return (
         <>
             <form onSubmit={DisplayTask} style={{border:"4px dotted pink"}}>
