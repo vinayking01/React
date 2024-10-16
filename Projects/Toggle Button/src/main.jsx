6755c5910359b0f646607f49d8f5a00dfdb2bfe5
Vinay Singh <vinaysinghcktd01@gmail.com>
2024-10-16 13:49:09 +0530

    code correction

--- Projects/Toggle Button/src/main.jsx
+++ Projects/Toggle Button/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
